6fc52eef5cb8224cddf58d2e329ab066b5c8cf53
gsure-tech <Geeescode@>
2023-09-06 02:44:16 +0100

    Swagger implementation and documentation

--- src/main/java/com/gsuretech/gsuretechbank/GsureTechBankApplication.java
+++ src/main/java/com/gsuretech/gsuretechbank/GsureTechBankApplication.java
@@ -1,9 +1,34 @@
 package com.gsuretech.gsuretechbank;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Quick Pay App",
+                description = "Backend Rest APIs for Quick Pay",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "Abdulganiyu Abubakar",
+                        email = "abubakarabdul9023@gmail.com",
+                        url = "https://github.com/Gsure-tech/Quickpay.git"
+                ),
+                license = @License(
+                        name = "Gsure-tech",
+                        url = "https://github.com/Gsure-tech/Quickpay.git"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Quick Pay App Documentation",
+                url = "https://github.com/Gsure-tech/Quickpay.git"
+        )
+)
 public class GsureTechBankApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/gsuretech/gsuretechbank/controller/UserController.java
+++ src/main/java/com/gsuretech/gsuretechbank/controller/UserController.java
@@ -5,26 +5,55 @@ import com.gsuretech.gsuretechbank.dto.CreditDebitRequest;
 import com.gsuretech.gsuretechbank.dto.EnquiryRequest;
 import com.gsuretech.gsuretechbank.dto.UserRequest;
 import com.gsuretech.gsuretechbank.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
+@Tag(name = "User Account Management APIs")
 public class UserController {
 
     @Autowired
     UserService userService;
 
+    @Operation(
+            summary = "Create New User Account",
+            description = "Creating a new user and assigning an account ID"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     @PostMapping
     public BankResponse createAccount(@RequestBody UserRequest userRequest){
         return userService.createAccount(userRequest);
     }
 
+
+    @Operation(
+            summary = "Balance Enquiry",
+            description = "Given an account number, check how much the user has"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping("/balanceEnquiry")
     public BankResponse balanceEnquiry(@RequestBody EnquiryRequest enquiryRequest){
         return userService.balanceEnquiry(enquiryRequest);
     }
 
+    @Operation(
+            summary = "Name Enquiry",
+            description = "Given an account number, Get the username of the account"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping("/nameEnquiry")
     public String nameEnquiry (@RequestBody EnquiryRequest enquiryRequest){
         return userService.nameEnquiry(enquiryRequest);
