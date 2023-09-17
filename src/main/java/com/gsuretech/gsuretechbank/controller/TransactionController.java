0bb18332277e75594434761ab7524f0c012a059d
gsure-tech <Geeescode@>
2023-09-17 23:11:29 +0100

    transaction message

---
+++ src/main/java/com/gsuretech/gsuretechbank/controller/TransactionController.java
@@ -0,0 +1,26 @@
+package com.gsuretech.gsuretechbank.controller;
+
+import com.gsuretech.gsuretechbank.entity.Transaction;
+import com.gsuretech.gsuretechbank.service.BankStatement;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bankStatement")
+@AllArgsConstructor
+public class TransactionController {
+    private BankStatement bankStatement;
+
+    @GetMapping
+    public List<Transaction> generateBankStatement(@RequestParam String accountNumber,
+                                                   @RequestParam String startDate,
+                                                   @RequestParam String endDate){
+        return bankStatement.generateStatement(accountNumber,startDate,endDate);
+    }
+
+}
--- src/main/java/com/gsuretech/gsuretechbank/service/BankStatement.java
+++ src/main/java/com/gsuretech/gsuretechbank/service/BankStatement.java
@@ -1,4 +1,34 @@
 package com.gsuretech.gsuretechbank.service;
 
+import com.gsuretech.gsuretechbank.entity.Transaction;
+import com.gsuretech.gsuretechbank.repository.TransactionRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+@Component
+@AllArgsConstructor
 public class BankStatement {
+
+    @Autowired
+    TransactionRepository transactionRepository;
+    /**
+     *retrieve list of transactions within a date range given an account number
+     * generate a pdf file of transactions
+     * send the file via email
+     */
+
+    public List<Transaction> generateStatement(String accountNumber, String startDate, String endDate){
+        LocalDate start = LocalDate.parse(startDate, DateTimeFormatter.ISO_DATE);
+        LocalDate end = LocalDate.parse(endDate, DateTimeFormatter.ISO_DATE);
+        List<Transaction> transactionList = transactionRepository.findAll().stream()
+                .filter(transaction -> transaction.getAccountNumber().equals(accountNumber))
+                .filter(transaction -> transaction.getCreatedAt().isEqual(start.atStartOfDay()))
+                .filter(transaction -> transaction.getCreatedAt().isEqual(end.atStartOfDay()))
+                .toList();
+        return transactionList;
+    }
 }
