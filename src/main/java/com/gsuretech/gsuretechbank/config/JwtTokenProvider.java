fd4e3587f74dfe09f44be6c22bb90c2b7f802f3a
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2023-10-06 06:47:23 +0100

    generating token

--- src/main/java/com/gsuretech/gsuretechbank/config/JwtTokenProvider.java
+++ src/main/java/com/gsuretech/gsuretechbank/config/JwtTokenProvider.java
@@ -1,8 +1,42 @@
 package com.gsuretech.gsuretechbank.config;
 
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
 
 @Component
 public class JwtTokenProvider {
+
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt-expiration}")
+    private long jwtExpirationDate;
+
+    public String generateToken(Authentication authentication){
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(currentDate)
+                .setExpiration(expireDate)
+                .signWith(key())
+                .compact();
+
+    }
+
+    private Key key(){
+        byte[] bytes = Decoders.BASE64.decode(jwtSecret);
+        return Keys.hmacShaKeyFor(bytes);
+    }
 }
